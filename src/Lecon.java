ecfb53b5206c0d0184db4c469ee9d4d0908a6d4c
Alen Bijelic <alen.bijelic@heig-vd.ch>
2021-11-04 23:51:06 +0100

    Ajout de l'affichage d'exemple des leçons dans l'horaire

--- src/Lecon.java
+++ src/Lecon.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Lecon {
     private static final String[] JOURS_SEMAINE = {"Lun", "Mar", "Mer", "Jeu", "Ven"};
     private static final String[] PERIODE_DEBUT = {"8:30", "9:15", "10:25", "11:15", "12:00", "13:15", "14:00", "14:55", "15:45", "16:35", "17:20"};
@@ -70,13 +72,29 @@ public class Lecon {
             // Ajout du padding avec l'horaire
             horaire.append(String.format(String.format("\n%%%ds", PADDING_HORAIRE), PERIODE_DEBUT[i] + LINE_CHAR));
             // Affichage d'une ligne où seront écrites les leçons
-            // TODO: Transformer en boucle pour pouvoir écrire la leçon
-            horaire.append(String.valueOf(emptyColumn).repeat(JOURS_SEMAINE.length));
+            for(int j = 0; j < JOURS_SEMAINE.length; ++j){
+                // TODO: Changer la condition pour vérifier si un cours se déroule pendant cette horaire
+                if(i == 2 && j == 3 || i == 0 && j == 0){
+                    // TODO: Changer les données de leconFormat pour afficher les vraies informations
+                    String leconFormat = String.format("%s   %s %s", "POO", "H02", "PDO");
+                    horaire.append(String.format(String.format("%%-%ds%c", COLUMN_WIDTH, LINE_CHAR), leconFormat));
+                }
+                else{
+                    horaire.append(emptyColumn);
+                }
+            }
             // Ajout du padding sans horaire
             horaire.append(emptyPadding);
             // Affichage d'une ligne complète
-            // TODO: Transformer en boucle pour pouvoir enlever les traietillé si une leçon s'étend sur plusieurs périodes
-            horaire.append(String.valueOf(fullColumn).repeat(JOURS_SEMAINE.length));
+            for(int j = 0; j < JOURS_SEMAINE.length; ++j){
+                // TODO: modifier la condition pour vérifier si une leçon s'étend sur plusieurs périodes
+                if(i == 2 && j == 3){
+                    horaire.append(emptyColumn);
+                }
+                else{
+                    horaire.append(fullColumn);
+                }
+            }
 
         }
 
