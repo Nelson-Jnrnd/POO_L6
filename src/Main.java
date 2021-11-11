81d027bb06d9ae9ca3a5b72da59677a11b9cb58b
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2021-11-11 15:39:46 +0100

    merge

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ public class Main {
         Professeur Rossier = new Professeur("Rossie", "Daniel", "DRE");
         Professeur Donini = new Professeur("Donini", "Pier", "PDO");
 
-        Lecon POO = new Lecon("POO", "H02", 4, 5, 2);
-        Lecon SYE = new Lecon("SYE", "H01", 4, 7, 3);
-        Lecon TIC = new Lecon("TIC", "A23", 0, 1, 1);
+        Lecon POO1 = new Lecon("POO", "H02", 2, 5, 2);
+        Lecon POO2 = new Lecon("POO", "H02", 3, 5, 2);
+        Lecon POO3 = new Lecon("POO", "H02", 3, 7, 2);
+        Lecon SYE1 = new Lecon("SYE", "G01", 0, 0, 2);
+        Lecon SYE2 = new Lecon("SYE", "H01", 3, 2, 2);
+        Lecon TIC = new Lecon("TIC", "F06", 1, 9, 1);
 
         Etudiant Lennon = new Etudiant("Lennon", "John", 1234);
         Etudiant McCartney = new Etudiant("Mc Cartney", "Paul", 1234);
@@ -15,7 +18,7 @@ public class Main {
 
         Groupe IL6_1 = new Groupe("IL6-1", "IL", 1, 6, new Etudiant[]{Lennon, McCartney, Starr, Harisson});
         Groupe SI6_1 = new Groupe("SI6-1", "SI", 1, 6, new Etudiant[]{Harisson, Gilmour});
-
+        IL6_1.definirLecons(new Lecon[]{SYE1, SYE2, POO1, POO2, POO3, TIC});
         Personne[] heig = new Personne[]{Rossier, Donini, Lennon, McCartney, Starr, Harisson, Gilmour};
 
         System.out.println("-- Membre de l'école");
